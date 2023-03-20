68e4b0ffd6481d9f51aeafe2b6c9d57cc811fc98
Gidwin <gidwin@mail.ru>
2023-03-20 19:45:44 +0300

    fix send to all conections

--- task/main.py
+++ task/main.py
@@ -3,7 +3,6 @@ from fastapi.responses import FileResponse
 import json
 
 app = FastAPI()
-connections = []
 
 @app.get("/")
 async def get():
@@ -12,15 +11,10 @@ async def get():
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    connections.append(websocket)
     number = 0
-    try:
-        while True:
-            data = await websocket.receive_text()
-            message = json.loads(data)
-            number += 1
-            response = {"number": number, "message": message["message"]}
-            for connection in connections:
-                await connection.send_json(response)
-    except:
-        connections.remove(websocket)
+    while True:
+        data = await websocket.receive_text()
+        message = json.loads(data)
+        number += 1
+        response = {"number": number, "message": message["message"]}
+        await websocket.send_json(response)
